c092f9f3d355f28e9b5144b6fc8b6f5f50520bf8
volodymyr.k <volodymyr.k@playtika.com>
2020-07-27 20:08:53 +0300

    Add cli description from package.json

--- src/bin.js
+++ src/bin.js
@@ -7,7 +7,7 @@ const program = new commander.Command();
 
 program
   .version(pjson.version)
-  .description('A cli tool that copy and paste directory files and replace text occurrences in files content and path in different cases.')
+  .description(pjson.description)
   .requiredOption('--src <src>', 'The directory absolute path to copy')
   .requiredOption('--dst <dst>', 'The directory absolute path that will contain the copied one')
   .requiredOption('--from <from>', 'Word to replace, preferable lowercase separated with spaces')
